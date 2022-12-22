5fa75c941763d6bccd593c19fbedbc1088c2b612
gilang <initial.gr.101@gmail.com>
2022-12-22 10:14:25 +0700

    coba-bwastore

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
+
+class CheckoutController extends Controller
+{
+    public function process(Request $request)
+    {
+        // Save User Data
+        $user = Auth::user();
+        $user->update($request->except('total_price'));
+
+        // Proses Checkout
+        $code = 'STORE-' . mt_rand(0000, 9999);
+        $carts = Cart::with(['product', 'user'])
+            ->where('users_id', Auth::user()->id)
+            ->get();
+
+        // Transaction Create
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'insurance_price' => 0,
+            'shipping_price' => 0,
+            'total_price' => $request->total_price,
+            'transaction_status' => 'PENDING',
+            'code' => $code,
+        ]);
+
+        foreach ($carts as $cart) {
+            $trx = 'TRX-' . mt_rand(0000, 9999);
+
+            TransactionDetail::create([
+                'transactions_id' => $transaction->id,
+                'products_id' => $cart->product->id,
+                'price' => $cart->product->price,
+                'shipping_status' => 'PENDING',
+                'resi' => '',
+                'code' => $trx,
+            ]);
+        }
+
+        // Delete Cart data
+        Cart::where('users_id', Auth::user()->id)->delete();
+
+        // Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config();
+        Config::$is3ds = true;
+
+        // Buat Array untuk dikirim ke midtrans
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => $code,
+                'gross_amount' => (int) $request->total_price,
+            ],
+            'customers_details' => [
+                'first_name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+            ],
+            'enabled_payments' => [
+                'gopay', 'shopeepay'
+            ],
+            'vtweb' => []
+        ];
+
+        try {
+            // Get Snap Payment Page URL
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+
+            // Redirect to Snap Payment Page
+            return redirect($paymentUrl);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function callback(Request $request)
+    {
+    }
+}
